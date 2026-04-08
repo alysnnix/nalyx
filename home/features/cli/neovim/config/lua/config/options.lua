ae966ad813f8a7e247e2e27469a6cfb7164b306b
aly <4ly.alcantara@gmail.com>
2026-04-08 23:13:20 +0000

    feat(neovim): add editor options and autocmds (binary file opener)

---
+++ home/features/cli/neovim/config/lua/config/options.lua
@@ -0,0 +1,36 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- General
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+-- UI
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Wrapping
+vim.opt.wrap = false
