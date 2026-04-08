6514143c0c02c409bb6b82c9f16adcf00bf8a2e8
aly <4ly.alcantara@gmail.com>
2026-04-08 23:14:55 +0000

    feat(neovim): add neo-tree and telescope customization

---
+++ home/features/cli/neovim/config/lua/plugins/editor.lua
@@ -0,0 +1,39 @@
+return {
+  -- Neo-tree customization (already included by LazyVim)
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "left",
+        width = 30,
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      filesystem = {
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    },
+  },
+
+  -- Telescope customization (already included by LazyVim)
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            preview_width = 0.55,
+          },
+        },
+      },
+    },
+  },
+}
