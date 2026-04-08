80158e5c01fb3760b2dd37caec3c7a294c3dc333
aly <4ly.alcantara@gmail.com>
2026-04-08 23:14:58 +0000

    feat(neovim): add LSP config with Mason disabled, Nix-managed servers

---
+++ home/features/cli/neovim/config/lua/plugins/lsp.lua
@@ -0,0 +1,50 @@
+return {
+  -- Disable Mason (LSP servers installed via Nix)
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+
+  -- LSP configuration
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ts_ls = {},
+        pyright = {},
+        gopls = {},
+        jdtls = {},
+        nil_ls = {},
+        texlab = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              completion = { callSnippet = "Replace" },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- Disable conform.nvim Mason integration, formatters are on PATH via Nix
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        javascriptreact = { "prettier" },
+        json = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        python = { "black" },
+        go = { "gofmt" },
+        lua = { "stylua" },
+        tex = { "latexindent" },
+        latex = { "latexindent" },
+      },
+    },
+  },
+}
