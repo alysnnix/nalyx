cdd63a01943bee8f215e931e39b99b08c848558a
aly <4ly.alcantara@gmail.com>
2026-04-08 23:17:48 +0000

    feat(neovim): rewrite module with xdg.configFile and full LSP/formatter packages

--- home/features/cli/neovim/default.nix
+++ home/features/cli/neovim/default.nix
@@ -1,47 +1,44 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     neovim
+
+    # Telescope dependencies
     ripgrep
     fd
+
+    # LSP servers
     nodePackages.typescript-language-server
     pyright
     gopls
+    jdt-language-server
     nil
+    texlab
     lua-language-server
+
+    # Formatters
     nixfmt
     nodePackages.prettier
     black
     stylua
+    texlivePackages.latexindent
+
+    # Build dependency for avante.nvim
+    gcc
+    gnumake
   ];
 
-  # Deploy LazyVim config
-  home.file."${config.home.homeDirectory}/.config/nvim/init.lua".text = ''
-    -- Bootstrap lazy.nvim
-    local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-    if not (vim.uv or vim.loop).fs_stat(lazypath) then
-      vim.fn.system {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--depth=1",
-        lazypath,
-      }
-    end
-    vim.opt.rtp:prepend(lazypath)
-
-    -- Load LazyVim
-    require("lazy").setup("lazyvim.plugins")
-  '';
-
-  home.file."${config.home.homeDirectory}/.config/nvim/lazyvim.json".text = ''
-    {
-      "extras": true
-    }
-  '';
+  # Deploy Lua config to ~/.config/nvim/
+  xdg.configFile = {
+    "nvim/init.lua".source = ./config/init.lua;
+    "nvim/lazyvim.json".source = ./config/lazyvim.json;
+    "nvim/lua/config/options.lua".source = ./config/lua/config/options.lua;
+    "nvim/lua/config/keymaps.lua".source = ./config/lua/config/keymaps.lua;
+    "nvim/lua/config/autocmds.lua".source = ./config/lua/config/autocmds.lua;
+    "nvim/lua/plugins/ui.lua".source = ./config/lua/plugins/ui.lua;
+    "nvim/lua/plugins/editor.lua".source = ./config/lua/plugins/editor.lua;
+    "nvim/lua/plugins/lsp.lua".source = ./config/lua/plugins/lsp.lua;
+    "nvim/lua/plugins/git.lua".source = ./config/lua/plugins/git.lua;
+    "nvim/lua/plugins/ai.lua".source = ./config/lua/plugins/ai.lua;
+  };
 }
