eb77f4500535c99fab38f456a8e45e6023a6fa6a
aly <4ly.alcantara@gmail.com>
2026-04-09 05:06:25 +0000

    feat(hyprland): update matugen templates -- drop rofi/waybar, enable kitty

--- home/features/desktop/hyprland/matugen/default.nix
+++ home/features/desktop/hyprland/matugen/default.nix
@@ -5,46 +5,25 @@
     swww
   ];
 
-  # 1. Copia a pasta de templates que você baixou do Dusky
   xdg.configFile."matugen/templates" = {
     source = ./templates;
     recursive = true;
   };
 
-  # 2. Gera o config.toml adaptado
   xdg.configFile."matugen/config.toml".text = ''
     [config]
     reload_on_change = true
 
-    # --- HYPRLAND ---
+    # Hyprland border colors
     [templates.hyprland]
     input_path = "~/.config/matugen/templates/hyprland-colors.conf"
     output_path = "~/.config/hypr/colors.conf"
     post_hook = "hyprctl reload"
 
-    # --- ROFI ---
-    [templates.rofi]
-    input_path = "~/.config/matugen/templates/rofi-colors.rasi"
-    output_path = "~/.config/rofi/colors.rasi"
-    # O Rofi recarrega sozinho na próxima vez que abrir
-
-    # --- WAYBAR ---
-    [templates.waybar]
-    input_path = "~/.config/matugen/templates/colors.css"
-    output_path = "~/.config/waybar/colors.css"
-    # Recarrega a Waybar suavemente
-    post_hook = "pkill -SIGUSR2 waybar || true"
-
-    # --- KITTY (Descomente se você usar o Kitty) ---
-    # [templates.kitty]
-    # input_path = "~/.config/matugen/templates/kitty-colors.conf"
-    # output_path = "~/.config/kitty/colors.conf"
-    # post_hook = "pkill -SIGUSR1 kitty || true"
-
-    # --- CAVA (Visualizer de Audio) ---
-    # [templates.cava]
-    # input_path = "~/.config/matugen/templates/cava-colors.ini"
-    # output_path = "~/.config/cava/themes/cava-colors.ini"
-    # post_hook = "pkill -USR1 cava || true"
+    # Kitty terminal colors
+    [templates.kitty]
+    input_path = "~/.config/matugen/templates/kitty-colors.conf"
+    output_path = "~/.config/kitty/colors.conf"
+    post_hook = "pkill -SIGUSR1 kitty || true"
   '';
 }
